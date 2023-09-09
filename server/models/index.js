f9517f2744b15ab77c34bac532726f45fdeae680
allan-paz <allan.paz@laureate.net>
2023-09-09 10:44:46 -0600

    Add envs configs

--- server/models/index.js
+++ server/models/index.js
@@ -4,9 +4,12 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
+require('dotenv').config(); // this is important!
+
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const env = process.env.NODE_ENV || 'pod';
+console.log('current node env: ', process.env.NODE_ENV)
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
