ffdcb03084143ae4c27df358381a154331d7c42b
allan-paz <allan.paz@laureate.net>
2023-09-12 13:56:49 -0600

    get incomes and create incomes endpoint created

--- server/controllers/Incomes.js
+++ server/controllers/Incomes.js
@@ -1,14 +1,109 @@
-const { Incomes } = require('../models');
+const { Incomes, Budgets } = require('../models');
 
 module.exports = {
     async getIncomes(req, res) {
-        console.log('Retrieving user incomes ...')
+        console.log('Retrieving user incomes ...', req.query)
+
+        const { user, budget, pages, sort } = req.query
+
+        if (!user || !budget) {
+            return res.status(404).json({
+                message: 'No income data sent',
+                code: 200,
+                messageCode: 'NOTFOUND',
+                data: [],
+                pages: 6,
+            })
+        }
+
+        const query = {
+            where: { user, budget },
+            order: [
+                ['createdAt', 'ASC']
+            ]
+        }
+        if (pages) query.offset = parseInt(pages)
+        if (sort) query.limit = parseInt(sort)
+        console.log('query', query)
+        const { rows, count } = await Incomes.findAndCountAll(query)
+        console.log('Budget data retrieved: ', count)
+        let my_budgets = rows
+        if (!pages & !sort) {
+            my_budgets = rows.slice(0, 6);
+        }
+        if (rows.length > 0) {
+            return res.status(200).json({
+                message: 'Budgets retrieved',
+                code: 200,
+                messageCode: 'GetBudgets',
+                data: my_budgets,
+                pages: count === 0 ? 6 : count,
+            })
+        } else {
+            return res.status(200).json({
+                message: 'No budgets found',
+                code: 200,
+                messageCode: 'GetBudgets',
+                data: [],
+                pages: 6,
+            })
+        }
     },
     async updateIncomes(req, res) {
         console.log('Updating user incomes ...')
     },
     async createIncomes(req, res) {
-        console.log('Creating user incomes ...')
+        console.log('Creating user incomes ...', req.body)
+        const { name, user, total, description, budget, date } = req.body
+        if (!user || !total || !description || !name || !budget || !date) {
+            return res.status(301).json({
+                message: 'Invalid income data.',
+                code: 301,
+                messageCode: 'INVALIDINCOME',
+                data: []
+            })
+        }
+
+
+        const { dataValues: newIncome } = await Incomes.create({
+            name, user, total: parseInt(total), description, budget, date
+        })
+
+        if (newIncome) {
+            console.log('New Income created', newIncome)
+
+            const query = {
+                where: { user, name: budget }
+            }
+            const incomeBudget = await Budgets.findOne(query)
+
+            if (incomeBudget) {
+                const updateBudgetBalance = await Budgets.update({ balance: parseInt(incomeBudget.dataValues.balance) + parseInt(total) },
+                    { where: { name: budget, user } })
+                    if (updateBudgetBalance) {
+                        return res.status(200).json({
+                            message: 'Income successfully created',
+                            code: 200,
+                            messageCode: 'CreateIncome',
+                            data: []
+                        })
+                    }
+            }
+            return res.status(301).json({
+                message: 'Budget balance not updated',
+                code: 301,
+                messageCode: 'NOTUPDATED',
+                data: []
+            })
+        }
+
+
+        return res.status(301).json({
+            message: 'Budget was not created',
+            code: 200,
+            messageCode: 'CreateBudget',
+            data: []
+        })
     },
     async deleteIncomes(req, res) {
         console.log('Deleting user incomes ...')
